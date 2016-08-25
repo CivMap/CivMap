4bc38ee6cdbbe5ec28b39351c5731b99d5d0c4a5
Gjum <code.gjum@gmail.com>
2016-08-25 03:14:27 +0200

    add favicon

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ import os
 import json
 from flask import Flask, Response, request
 
-app = Flask(__name__)#, static_url_path='', static_folder="static")
-# app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
+app = Flask(__name__)
+app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
-
-@app.route('/', methods=['GET'])
-def index():
-  return app.send_static_file('index.html')
+@app.route('/<path:path>')
+def static_path(path):
+  return app.send_static_file(path)
 
 @app.route('/todos.json', methods=['GET', 'POST'])
 def todo():
@@ -18,10 +17,6 @@ def todo():
         todos = request.get_json()
 
     return Response(json.dumps(todos), mimetype='application/json')
-
-@app.route('/<path:path>')
-def static_path(path):
-  return app.send_static_file(path)
 
 if __name__ == '__main__':
   port = int(os.environ.get("PORT", 5000))
