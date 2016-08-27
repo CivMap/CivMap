d78807bf3585b831b470fdeff52dbe9d756f07db
Gjum <code.gjum@gmail.com>
2016-08-27 22:08:32 +0200

    render tiles (only zoom=0), geojson, image (not offset correctly)

--- js/app.jsx
+++ js/app.jsx
@@ -1,21 +1,71 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var { Map, Marker, Popup, TileLayer } = require('react-leaflet');
+var L = require('leaflet');
+var RL = require('react-leaflet');
+var geoJsonTestData = require('./GeoJsonTestData.js');
 
-const position = [51.505, -0.09];
-const map = (
-  <Map className="map" key="map1"
-      center={position} zoom={13}>
-    <TileLayer
-      url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    />
-    <Marker position={position}>
-      <Popup>
-        <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-      </Popup>
-    </Marker>
-  </Map>
+var mcCRS = L.extend({}, L.CRS.Simple, {
+  transformation: new L.Transformation(1, 0, 1, 0)
+});
+
+function xz(x, z) {
+  return [z, x];
+}
+
+var CivMap = React.createClass({
+  render: function() {
+    var bounds = [xz(-1280, -1280), xz(1280, 1280)];
+    return (
+      <RL.Map
+          className="map"
+          crs={mcCRS}
+          center={[0, 0]}
+          maxBounds={bounds}
+          minZoom={0}
+          maxZoom={0}
+          zoom={0}
+          >
+        <RL.LayersControl position='topright'>
+          <RL.LayersControl.BaseLayer name='tiles' checked={true}>
+            <RL.TileLayer
+              url={'/tiles/'+this.props.name+'/{z}/{x}_{y}.png'}
+              tileSize={256}
+              bounds={bounds}
+              maxNativeZoom={0}
+              continuousWorld={true}
+              />
+          </RL.LayersControl.BaseLayer>
+          <RL.LayersControl.BaseLayer name='full img'>
+            <RL.ImageOverlay
+              url={'/maps/'+this.props.name+'.png'}
+              bounds={[xz(-1000, -1000), xz(1000, 1000)]}
+              />
+          </RL.LayersControl.BaseLayer>
+
+          <RL.LayersControl.Overlay name='geojson' checked={true}>
+            <RL.GeoJson data={this.props.geojson} />
+          </RL.LayersControl.Overlay>
+          <RL.LayersControl.Overlay name='marker' checked={true}>
+            <RL.LayerGroup>
+              <RL.Marker position={xz(775, -76)} title='Aquila'>
+                <RL.Popup>
+                  <span>Aquila center</span>
+                </RL.Popup>
+              </RL.Marker>
+              <RL.Marker position={[0, 0]} title={this.props.name}>
+                <RL.Popup>
+                  <span>Center of {this.props.name}</span>
+                </RL.Popup>
+              </RL.Marker>
+            </RL.LayerGroup>
+          </RL.LayersControl.Overlay>
+        </RL.LayersControl>
+      </RL.Map>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CivMap name='naunet' geojson={geoJsonTestData} />,
+  document.getElementById('content')
 );
-
-ReactDOM.render(map, document.getElementById('content'));
---
+++ py/tiles.py
@@ -0,0 +1,110 @@
+# generate leaflet tiles from VoxelMap cache
+import os
+import sys
+import time
+from collections import defaultdict
+import struct
+import cairo
+from zipfile import ZipFile
+
+shards = {
+    '44f4b133-a646-461a-a14a-5fd8c8dbc59c': 'tjikko',
+    'a7cbf239-6c11-4146-a715-ef0a9827b4c4': 'dracontas',
+    '197e2c4f-2fd6-464a-8754-53b24d9f7898': 'isolde',
+    'b25abb31-fd1e-499d-a5b5-510f9d2ec501': 'volans',
+    'a358b10c-7041-40c5-ac5e-db5483a9dfc2': 'tigrillo',
+    'a72e4777-ad62-4e3b-a4e0-8cf2d15147ea': 'rokko',
+    'de730958-fa83-4e73-ab7f-bfdab8e27960': 'naunet',
+    '7f03aa4d-833c-4b0c-9d3b-a65a5c6eada0': 'ulca',
+}
+
+region_size = 16*16  # blocks per region edge
+
+item_colors = {}
+for line in open('item-colors'):
+    id_tup, color = map(lambda e: e.split(','), line.split(':'))
+    item_id = int(id_tup[0]) | int(id_tup[1]) << 12
+    item_colors[item_id] = tuple(map(lambda c: int(c) / 255, color[:3]))
+
+missing_blocks = defaultdict(int)
+def print_missing_blocks():
+    for item_id, n in missing_blocks.items():
+        print('could not find color for %6s %3i:%-2i %ix'
+            % (hex(item_id), item_id & 0xfff, item_id >> 12, n))
+
+def get_block_color(item_id):
+    color = item_colors.get(item_id)
+    if color is None:
+        color = item_colors.get(item_id & 0x0fff)
+        if color is None:
+            missing_blocks[item_id] += 1
+            return (1,0,1)
+    return color
+
+def get_shard_id(name):
+    for sid, sname in filter(lambda e: name in e[1].lower(), shards.items()):
+        return sid
+    return None
+
+def civ_world_path(base_path, world_id=None, name=None):
+    if world_id is None: world_id = get_shard_id(name)
+    return base_path + '/cache/mc.civcraft.co/' + world_id + '/Overworld (dimension 0)/'
+
+def get_region_file_from_zip(zip_path):
+    return ZipFile(zip_path).open('data')
+
+def get_world_regions(world_path):
+    return [tuple(map(int, region[:-4].split(','))) for region in os.listdir(world_path)]
+
+
+class TileRenderer(object):
+    def __init__(self, world_path, tiles_path):
+        self.world_path = world_path
+        self.tiles_path = tiles_path
+        os.makedirs(self.tiles_path, exist_ok=True)
+
+    def render_tiles(self, print_progress=True):
+        regions = get_world_regions(self.world_path)
+        last_progress = time.time()
+        for rn, (rx, rz) in enumerate(regions):
+            if print_progress and last_progress + 3 < time.time():
+                last_progress += 3
+                print('%i/%i tiles' % (rn, len(regions)))
+            self.render_tile(rx, rz)
+
+    def render_tile(self, rx, rz):
+        region_file = get_region_file_from_zip(self.world_path + '/%i,%i.zip' % (rx, rz))
+        surf = cairo.ImageSurface(cairo.FORMAT_ARGB32, region_size, region_size)
+        ctx = cairo.Context(surf)
+
+        for z in range(region_size):
+            for x in range(region_size):
+                column = region_file.read(17)
+                if column[0] == 0: continue  # no data present
+
+                ctx.set_source_rgba(*self.get_pixel_color(column, x, z))
+                ctx.rectangle(x, z, 1, 1)
+                ctx.fill()
+
+        surf.write_to_png('%s/%i_%i.png' % (self.tiles_path, rx, rz))
+
+class BlockRenderer(TileRenderer):
+    def get_pixel_color(self, column, x, z):
+        level1, level2, level3, level4, biome = struct.unpack('>IIIIB', column)
+
+        block_id = level1 >> 8 & 0xffff
+        y = level1 >> 24
+        if block_id == 0:
+            block_id = level4 >> 8 & 0xffff
+            y = level4 >> 24
+
+        shade = 1 - (((256-y)/4)%3)*.02
+        r, g, b = get_block_color(block_id)
+        r, g, b = r * shade, g * shade, b * shade
+
+        if level2:  # underwater
+            block_id = level2 >> 8 & 0xffff
+            r, g, b = get_block_color(block_id)
+            b = b*.6 + .4
+
+        return r, g, b
