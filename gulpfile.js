d1b036ad9d7141f50699cd33d26632d1b886d6c0
Gjum <code.gjum@gmail.com>
2016-08-28 13:29:05 +0200

    use babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
+var babel = require('gulp-babel');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
 
 gulp.task('browserify', function() {
   return gulp.src('js/app.jsx', {read: false})
+    .pipe(babel({
+      presets: ['es2015']
+    }))
     .pipe(browserify({
       transform : 'reactify',
       extensions: ['.jsx']
