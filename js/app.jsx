50d2b569ba1512b567f6fcddf50e1d35bb0f8b87
Gjum <code.gjum@gmail.com>
2016-09-02 17:41:03 +0200

    move utils to util.js

--- js/app.jsx
+++ js/app.jsx
@@ -3,79 +3,34 @@ var ReactDOM = require('react-dom');
 var L = require('leaflet');
 var RL = require('react-leaflet');
 
-RL.setIconDefaultImagePath('/leaflet-dist/images');
+var Util = require('./util.js');
 
-const attribution = '<a href="https://github.com/CivMap">Civcraft Mapping Agency</a>'
-  + ' | Visit civmap.herokuapp.com'
-  + ' | <a href="https://github.com/CivMap/civmap">contribute</a>';
+RL.setIconDefaultImagePath('/leaflet-dist/images');
 
 const dataRoot = 'https://raw.githubusercontent.com/CivMap/civ3-data/master/';
 
 const emptyImg = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
 
-function getJSON(url, onData, onErr) {
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.onreadystatechange = function() {
-    if (this.readyState === 4) {
-      if (this.status >= 200 && this.status < 400) {
-        onData(JSON.parse(this.responseText));
-      } else {
-        onErr && onErr(this);
-      }
-    }
-  };
-  request.send();
-  request = null;
-}
-
 var mcCRS = L.extend({}, L.CRS.Simple, {
   transformation: new L.Transformation(1, 0, 1, 0)
 });
-
-function xz(x, z) {
-  return [z, x];
-}
-
-function viewToHash(leaf, worldName) {
-  var center = leaf.getCenter();
-  return  '' + worldName + '/' + center.lng + 'x/' + center.lat + 'z/' + leaf.getZoom() + 'zoom';
-}
-
-function hashToView(hash) {
-  if (!hash) return {worldName: 'dracontas', x: 0, z: 0, zoom: 0}; // default world if no hash
-  var [worldName, x, z, zoom] = hash.slice(1).split('/', 4)
-    .concat([0,0,0]); // default coords/zoom if not in url
-  return {worldName: worldName, x: parseFloat(x), z: parseFloat(z), zoom: parseFloat(zoom)};
-}
-
-function makeBounds(bounds) {
-  return[xz(bounds.min_x, bounds.min_z), xz(bounds.max_x, bounds.max_z)];
-}
-
-function getWorld(worlds, worldName, defaultWorld) {
-  if (!worldName) return defaultWorld || worlds[0];
-  var activeWorld = worlds.filter(w => w.name === worldName)[0];
-  if (!activeWorld) return defaultWorld || worlds[0]; // unknown world
-  return activeWorld;
-}
 
 class CivMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeWorld: getWorld(props.worlds, props.initialView.worldName),
+      activeWorld: Util.getWorld(props.worlds, props.initialView.worldName),
     };
   }
 
   onbaselayerchange(o) {
-    this.setState({activeWorld: getWorld(this.props.worlds, o.name, this.state.activeWorld)});
+    this.setState({activeWorld: Util.getWorld(this.props.worlds, o.name, this.state.activeWorld)});
     this.updateHash(o);
   }
 
   updateHash(o) {
     if (this.state.activeWorld && 'name' in this.state.activeWorld)
-      location.hash = viewToHash(o.target, this.state.activeWorld.name);
+      location.hash = Util.viewToHash(o.target, this.state.activeWorld.name);
   }
 
   render() {
@@ -83,8 +38,8 @@ class CivMap extends React.Component {
       <RL.Map
           className="map"
           crs={mcCRS}
-          maxBounds={makeBounds(this.state.activeWorld.bounds)}
-          center={xz(this.props.initialView.x, this.props.initialView.z)}
+          maxBounds={Util.makeBounds(this.state.activeWorld.bounds)}
+          center={Util.xz(this.props.initialView.x, this.props.initialView.z)}
           zoom={this.props.initialView.zoom}
           maxZoom={5}
           minZoom={0}
@@ -98,11 +53,11 @@ class CivMap extends React.Component {
                   key={'tilelayer-' + world.name}
                   checked={world.name === this.state.activeWorld.name}>
                 <RL.TileLayer
-                  attribution={attribution}
+                  attribution={Util.attribution}
                   url={dataRoot+'tiles/'+world.name+'/{x}_{y}.png'}
                   errorTileUrl={emptyImg}
                   tileSize={256}
-                  bounds={makeBounds(world.bounds)}
+                  bounds={Util.makeBounds(world.bounds)}
                   minZoom={0}
                   maxNativeZoom={0}
                   continuousWorld={true}
@@ -131,10 +86,10 @@ class CivMap extends React.Component {
   }
 }
 
-getJSON(dataRoot+'meta/worlds.json', function(worlds) {
+Util.getJSON(dataRoot+'meta/worlds.json', function(worlds) {
   worlds = worlds.filter((w) => 'bounds' in w); // ignore incomplete world data
   ReactDOM.render(
-    <CivMap worlds={worlds} initialView={hashToView(location.hash)} />,
+    <CivMap worlds={worlds} initialView={Util.hashToView(location.hash)} />,
     document.getElementById('civmap')
   );
 });
---
+++ js/util.js
@@ -0,0 +1,56 @@
+const attribution = '<a href="https://github.com/CivMap">Civcraft Mapping Agency</a>'
+  + ' | Visit civmap.herokuapp.com'
+  + ' | <a href="https://github.com/CivMap/civmap">contribute</a>';
+
+function getJSON(url, onData, onErr) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.onreadystatechange = function() {
+    if (this.readyState === 4) {
+      if (this.status >= 200 && this.status < 400) {
+        onData(JSON.parse(this.responseText));
+      } else {
+        onErr && onErr(this);
+      }
+    }
+  };
+  request.send();
+  request = null;
+}
+
+function xz(x, z) {
+  return [z, x];
+}
+
+function viewToHash(leaf, worldName) {
+  var center = leaf.getCenter();
+  return  '' + worldName + '/' + center.lng + 'x/' + center.lat + 'z/' + leaf.getZoom() + 'zoom';
+}
+
+function hashToView(hash) {
+  if (!hash) return {worldName: 'dracontas', x: 0, z: 0, zoom: 0}; // default world if no hash
+  var [worldName, x, z, zoom] = hash.slice(1).split('/', 4)
+    .concat([0,0,0]); // default coords/zoom if not in url
+  return {worldName: worldName, x: parseFloat(x), z: parseFloat(z), zoom: parseFloat(zoom)};
+}
+
+function makeBounds(bounds) {
+  return[xz(bounds.min_x, bounds.min_z), xz(bounds.max_x, bounds.max_z)];
+}
+
+function getWorld(worlds, worldName, defaultWorld) {
+  if (!worldName) return defaultWorld || worlds[0];
+  var activeWorld = worlds.filter(w => w.name === worldName)[0];
+  if (!activeWorld) return defaultWorld || worlds[0]; // unknown world
+  return activeWorld;
+}
+
+module.exports = {
+  attribution: attribution,
+  getJSON: getJSON,
+  xz: xz,
+  viewToHash: viewToHash,
+  hashToView: hashToView,
+  makeBounds: makeBounds,
+  getWorld: getWorld,
+}
